ecc591b56bb07be47a5ba5195c907d5b8097db72
Richard Yun <ryun@Richies-MacBook-Pro.local>
2017-04-08 19:28:19 -0400

    Added checkbox search, reorganized code into partials and  added JS to make all checkbox functionality work

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,29 +1,27 @@
 class PhonesController < ApplicationController
   def index
-    @brand_name = Phone.new
     @brand_names = Phone.select(:brand_name).distinct.order(:brand_name)
     @operating_systems = Phone.select(:os).distinct
     @price_categories = Phone.select(:price_category).distinct.order(price_category: :asc)
- # ANDbrand_phone need unique
   end
 
   def search_results
-    puts "This is path_parameters" + ":#{request.POST}"
-    respond_to do |format|
-      format.html {render :partial => "search_results.html.erb"}
-      format.js {render("search_results.js.erb")
-      }
+    #If the all checkbox checked.
+    if params[:all].present?
+      @phones = Phone.all.order(:brand_name)
+    #If any checkbox besides all got checked.
+    elsif params[:brand_name].present?|| params[:os].present? || params[:price_category].present?
+      @phones = Phone.checkbox_search(request.POST)
+    #If searchbox form submitted.
+    elsif params[:search].present?
+      @phones = Phone.search_algorithm(params[:search])
+
+    else
     end
+
   end
 
-  def agnes
-    @thing = Phone.all
-    render(:layout => false)#, :locals => {:phones => @thing})
-  end
 
-  def testing
-  end
-  # end
   def show
     @phone = Phone.find(params[:id])
   end
--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,26 +1,15 @@
 class Phone < ApplicationRecord
   has_many :reviews
-  scope :search_algorithm, ->(search) do
-    #if search = 2 words and 2nd word = number, then treat as one word
-    #IF 3 LETTERS+ NOT A WORD --> RETURN NOTHING
-
+  def self.search_algorithm(search)
+    unless search.length <= 2
     where([(["phone_name LIKE ? OR brand_name LIKE ?"] * search.split.length).join(' OR ')] +
     search.split.flat_map{|x|["%" + x + "%"]*2})
+    end
   end
 
   def self.checkbox_search(params_hash)
-
-    params_array = [:brand_name, :os, :price_category]
-    column_array = []
-    key_array = []
-    params_hash.each do |k,v|
-      if params_array.include?(k)
-         column_array << v
-         key_array << k
-         puts column_array + key_array
-      end
-    end
-  return  joined_keys = key_array.join(" in (?) AND ") + ", " + "#{column_array}"
+    where(params_hash.slice("brand_name", "os", "price_category"))
   end
+
 
 end
