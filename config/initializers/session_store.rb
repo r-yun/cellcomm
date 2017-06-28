db787b01a0070cae6841ccc231521b69eff78ec4
Richard Yun <ryun@Richies-MacBook-Pro.local>
2017-06-28 16:21:12 -0400

    Committed to github

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_cell_comp_session'
+Rails.application.config.session_store :cookie_store, key: '_cellcomm_session'
